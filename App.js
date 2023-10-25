a843a035cdeb810610698ef39264e9289870552d
stevent12x <steven.turner12x@gmail.com>
2023-10-25 09:37:26 -0500

    implement eslint and prettier

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Hello World</Text>
+      </View>
+      <Text style={{ margin: 16, borderWidth: 2, borderColor: 'red', padding: 16 }}>Another Text Component</Text>
+      <Button title="Tap Me!" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center'
+  }
 });
